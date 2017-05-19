579e93eaf6378c5ab5203b2bf27cc2a40bf96135
Scoutdrago3 <Drago7654321@gmail.com>
2017-05-19 14:00:51 -0400

    Updated final Genesis object use

--- src/com/kharboutli/genesisAPI/Genesis.java
+++ src/com/kharboutli/genesisAPI/Genesis.java
@@ -2,23 +2,13 @@ package com.kharboutli.genesisAPI;
 
 /*
  * Class to represent a full "Genesis object". This class will contain
- * the various pieces of student data that have been scraped from the
+ * the the main HTML for a student that has been scraped from the
  * Genesis Parent Portal. In order to construct an object of this type,
- * the constructor will make calls to authenticate the user, grab all
- * of the information from the portal and store them in instance
- * variables. This class will only have getters to return the data.
- * 
- * 
+ * the constructor will make calls to authenticate the user. This class
+ * will only have getters to return the data.
  */
 
 public class Genesis {
-	
-	private String firstName;
-	private String lastName;
-	private String preferedName;
-	
-	private int grade;
-	
 	
 	/*
 	 * Placeholder Default constructor
