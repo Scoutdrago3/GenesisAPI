9db494fab5d7f244230e650923dfe64e31141a06
Scoutdrago3 <Drago7654321@gmail.com>
2017-05-23 15:26:59 -0400

    Seperated Course into a standalone class

---
+++ src/com/kharboutli/genesisAPI/Course.java
@@ -0,0 +1,47 @@
+package com.kharboutli.genesisAPI;
+
+/*
+ * A course object which contains information and methods to
+ * pertaining to the information of a single course in the
+ * grade book.
+ */
+public class Course
+{
+	private String myCourseName;
+	private String myTeacherLastName;
+	private double myGrade;
+	private int myPeriod;
+	
+	public Course(String courseName, String teacherLastName, double grade, int period)
+	{
+		myCourseName = courseName;
+		myTeacherLastName = teacherLastName;
+		myGrade = grade;
+		myPeriod = period;
+	}
+	
+	public String getCourseName()
+	{
+		return myCourseName;
+	}
+	
+	public String getTeacher()
+	{
+		return myTeacherLastName;
+	}
+	
+	public double getGrade()
+	{
+		return myGrade;
+	}
+	
+	public int getPeriod()
+	{
+		return myPeriod;
+	}
+	
+	public String toString()
+	{
+		return "Name: " + myCourseName + ", Taught By: " + myTeacherLastName + ", Current Course Grade: " + myGrade;
+	}
+}
