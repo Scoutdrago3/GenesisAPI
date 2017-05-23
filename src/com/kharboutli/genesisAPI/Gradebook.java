755ccf6bee9a76a0b49510490bf8a96c824b718b
Scoutdrago3 <drago7654321@gmail.com>
2017-05-22 22:45:27 -0400

    Fleshed out major methods

--- src/com/kharboutli/genesisAPI/Gradebook.java
+++ src/com/kharboutli/genesisAPI/Gradebook.java
@@ -69,12 +69,34 @@ public class Gradebook {
 	
 	public double getAverage4Grade()
 	{
-		
+		double sum = 0;
+		for(int c = 0; c < myCourses.length; c++)
+		{
+			double grade = myCourses[c].getGrade();
+			if(grade >= 90) sum += 4.0;
+			else if(grade >= 86) sum += 3.5;
+			else if(grade >= 80) sum += 3;
+			else if(grade >= 76) sum += 2.5;
+			else if(grade >= 70) sum += 2;
+			else if(grade >= 65) sum += 1.5;
+			else if(grade >= 60) sum += 1;
+			else if(grade >= 56) sum += .5;
+			else sum += 0;
+			
+		}
+		return sum / myCourses.length;
 	}
 	
-	public Course getCourse()
+	public Course getCourse(String key)
 	{
-		
+		for(int c = 0; c < myCourses.length; c++)
+		{
+			for(int d = 0; d < myCourses[c].getCourseName().length() - key.length(); d++)
+			{
+				if(myCourses[c].getCourseName().substring(d, d + key.length()).equals(key)) return myCourses[c];
+			}
+		}
+		return null;
 	}
 	
 	public String toString()
