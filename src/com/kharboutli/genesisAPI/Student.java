0002469e02652b1c845d7c881cc0cca3fa357d13
Scoutdrago3 <Drago7654321@gmail.com>
2017-05-23 15:28:15 -0400

    Added methods and a constructor

--- src/com/kharboutli/genesisAPI/Student.java
+++ src/com/kharboutli/genesisAPI/Student.java
@@ -2,12 +2,37 @@ package com.kharboutli.genesisAPI;
 
 public class Student {
 	
-	private Gradebook gradebook;
-	private String firstName;
+	private Gradebook myGradebook;
+	private String myFirstName;
+	private String myLastName;
+	private int myGrade;
 	
-	
-	public Student()
+	public Student(String email, String password)
 	{
-		
+		Genesis gen = new Genesis(email, password);
+		myGradebook = new Gradebook(gen.generateCourses());
+		myFirstName = gen.findName().substring(0, gen.findName().indexOf(','));
+		myLastName = gen.findName().substring(gen.findName().indexOf(',') + 1);
+		myGrade = gen.findGrade();
+	}
+	
+	public Gradebook getGradebook()
+	{
+		return myGradebook;
+	}
+	
+	public String getFirstName()
+	{
+		return myFirstName;
+	}
+	
+	public String getLastName()
+	{
+		return myLastName;
+	}
+	
+	public int getGrade()
+	{
+		return myGrade;
 	}
 }
