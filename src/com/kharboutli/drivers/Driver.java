19506367c9b90b89845c15cf76f46547484ddb6b
Scoutdrago3 <Drago7654321@gmail.com>
2017-06-05 13:39:58 -0400

    Moved Driver to a new package for drivers

--- src/com/kharboutli/drivers/Driver.java
+++ src/com/kharboutli/drivers/Driver.java
@@ -1,9 +1,10 @@
-package com.kharboutli.genesisAPI;
+package com.kharboutli.drivers;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.kharboutli.genesisAPI.Student;
 
 public class Driver {
 	public static void main(String args[]) throws FailingHttpStatusCodeException, MalformedURLException, IOException
