cf161108c9b46b175028f3a856089a7fc3dd9c62
Scoutdrago3 <drago7654321@gmail.com>
2017-06-12 17:30:34 -0400

    edit...

--- src/com/kharboutli/drivers/Driver.java
+++ src/com/kharboutli/drivers/Driver.java
@@ -9,7 +9,7 @@ import com.kharboutli.genesisAPI.Student;
 public class Driver {
 	public static void main(String args[]) throws FailingHttpStatusCodeException, MalformedURLException, IOException
 	{
-		Student stud = new Student("jooleen14@yahoo.com", "Marwan#3");
+		Student stud = new Student("email", "pass");
 		System.out.println(stud.getFirstName());
 		System.out.println(stud.getGrade());
 		System.out.println(stud.getStudentID());
